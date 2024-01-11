1e7e90f149fc01d073325d31af15a8f21a86f0bd
KingFryRice <graysonbeatonrice@gmail.com>
2024-01-10 22:52:59 -0800

    Store player input and run one round of rps Prompt will ask for player choice and store input with random computer choice. Function will evaluate for winner based on boolean comparison of player and computer input. Results will be printed to console with brief string message of winner or tie.

--- script.js
+++ script.js
@@ -8,13 +8,28 @@ function getComputerChoice() {
     return computerChoices [Math.floor(Math.random() * computerChoices.length)];
 }
 
-// Establish players choice through prompt input
-    // Players entry is not case sensitive 
-
-
 // Create function to evaluate computer choice and player choice and declare a winner with string and display in console
+    // Establish players choice through prompt input
+    // Players entry is not case sensitive 
     // Ties are redos
 
+function playRound(playerSelection, computerSelection) {
+    if ((playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "rock" && computerSelection === "scissor") ||
+        (playerSelection === "scissor" && computerSelection === "paper")) {
+        return "Congrats, you won this round!";
+    } else if (playerSelection === computerSelection) {
+        return "Let's try that again."
+    } else {
+        return "I got you this round!";
+    }
+}    
+
+let playerSelection = prompt("Rock, Paper, Scissor ... Shoot!").toLowerCase();
+let computerSelection = getComputerChoice();
+
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
 
 // Loop game until computer or player wins at least 3 games
 
