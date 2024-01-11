0cbeb672e9df5864f056ad11a2c43841075b7f25
KingFryRice <graysonbeatonrice@gmail.com>
2024-01-10 21:58:45 -0800

    Layout psuedocode for rps game

---
+++ script.js
@@ -0,0 +1,18 @@
+// Establish array for computer choice
+
+
+// Create function for computer choice to randomly return array element
+
+
+// Establish players choice through prompt input
+    // Players entry is not case sensitive 
+
+
+// Create function to evaluate computer choice and player choice and declare a winner with string and display in console
+    // Ties are redos
+
+
+// Loop game until computer or player wins at least 3 games
+
+
+// Either player or computer declared winner after game is over
