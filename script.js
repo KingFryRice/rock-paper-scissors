9353b6e604ad0311a7e07d4d5d058d4008fee984
KingFryRice <graysonbeatonrice@gmail.com>
2024-01-11 23:12:03 -0800

    Format for readability

--- script.js
+++ script.js
@@ -7,27 +7,6 @@ function getComputerChoice() {
     return computerChoices [Math.floor(Math.random() * computerChoices.length)];
 }
 
-// Loop game until computer or player wins at least 3 games
-    // Either player or computer declared winner after game is over
-
-function game() {
-    do {
-        playerSelection = prompt("Rock, Paper, Scissor ... Shoot!").toLowerCase();
-        computerSelection = getComputerChoice();
-        playRound(playerSelection, computerSelection);
-        console.log("Computer chose: " + computerSelection);
-        console.log(playRound(playerSelection, computerSelection));
-        console.log("Player Score: " + playerScore + " | Computer Score " + computerScore);
-    } while (playerScore < 3 && computerScore < 3);
-}
-
-function endGame() {
-    if (playerScore === 3) {
-        console.log("Great job you've bested me! Refresh to play, I won't lose again!.");
-    } if (computerScore === 3) 
-        console.log("Better luck next time, refresh if you want to try again!");
-}
-
 // Create function to evaluate computer choice and player choice and declare a winner with string and display in console
 
 let playerSelection
@@ -48,6 +27,27 @@ function playRound(playerSelection, computerSelection) {
         return "I got you this round!";
     }
 }    
+
+// Loop game until computer or player wins at least 3 games
+    // Either player or computer declared winner after game is over
+
+function game() {
+    do {
+        playerSelection = prompt("Rock, Paper, Scissor ... Shoot!").toLowerCase();
+        computerSelection = getComputerChoice();
+        playRound(playerSelection, computerSelection);
+        console.log("Computer chose: " + computerSelection);
+        console.log(playRound(playerSelection, computerSelection));
+        console.log("Player Score: " + playerScore + " | Computer Score " + computerScore);
+    } while (playerScore < 3 && computerScore < 3);
+}
+    
+function endGame() {
+    if (playerScore === 3) {
+        console.log("Great job you've bested me! Refresh to play, I won't lose again!.");
+    } if (computerScore === 3) 
+        console.log("Better luck next time, refresh if you want to try again!");
+}
 
 game();
 endGame();
