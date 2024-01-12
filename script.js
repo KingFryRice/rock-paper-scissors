214476854038b790a5dc40046b5ddf0d079bd04d
KingFryRice <graysonbeatonrice@gmail.com>
2024-01-11 21:55:20 -0800

    Add score increment to rps round function

--- script.js
+++ script.js
@@ -1,37 +1,39 @@
 // Establish array for computer choice
+    // Create function for computer choice to randomly return array element
 
 const computerChoices = ["rock", "paper", "scissor"];
-
-// Create function for computer choice to randomly return array element
 
 function getComputerChoice() {
     return computerChoices [Math.floor(Math.random() * computerChoices.length)];
 }
 
+// Loop game until computer or player wins at least 3 games
+    // Either player or computer declared winner after game is over
+
+
+
 // Create function to evaluate computer choice and player choice and declare a winner with string and display in console
     // Establish players choice through prompt input
-    // Players entry is not case sensitive 
-    // Ties are redos
 
 function playRound(playerSelection, computerSelection) {
     if ((playerSelection === "paper" && computerSelection === "rock") ||
         (playerSelection === "rock" && computerSelection === "scissor") ||
         (playerSelection === "scissor" && computerSelection === "paper")) {
+        playerScore++;
         return "Congrats, you won this round!";
     } else if (playerSelection === computerSelection) {
         return "Let's try that again."
     } else {
+        computerScore++;
         return "I got you this round!";
     }
 }    
 
 let playerSelection = prompt("Rock, Paper, Scissor ... Shoot!").toLowerCase();
 let computerSelection = getComputerChoice();
+let playerScore = 0;
+let computerScore = 0;
 
-console.log(computerSelection);
+console.log("Computer chose: " + computerSelection);
 console.log(playRound(playerSelection, computerSelection));
-
-// Loop game until computer or player wins at least 3 games
-
-
-// Either player or computer declared winner after game is over
+console.log("Player Score: " + playerScore + " | Computer Score " + computerScore);
