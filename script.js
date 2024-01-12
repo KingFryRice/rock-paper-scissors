129ce3328732b00eb8069a0915c045dd8b98de5f
KingFryRice <graysonbeatonrice@gmail.com>
2024-01-11 23:06:36 -0800

    Call end of game function Function logs end of game message based on winner. Adjust several variables to call functions locally.

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ function getComputerChoice() {
 function game() {
     do {
         playerSelection = prompt("Rock, Paper, Scissor ... Shoot!").toLowerCase();
+        computerSelection = getComputerChoice();
         playRound(playerSelection, computerSelection);
         console.log("Computer chose: " + computerSelection);
         console.log(playRound(playerSelection, computerSelection));
@@ -20,11 +21,17 @@ function game() {
     } while (playerScore < 3 && computerScore < 3);
 }
 
+function endGame() {
+    if (playerScore === 3) {
+        console.log("Great job you've bested me! Refresh to play, I won't lose again!.");
+    } if (computerScore === 3) 
+        console.log("Better luck next time, refresh if you want to try again!");
+}
+
 // Create function to evaluate computer choice and player choice and declare a winner with string and display in console
-    // Establish players choice through prompt input
 
 let playerSelection
-let computerSelection = getComputerChoice();
+let computerSelection
 let playerScore = 0;
 let computerScore = 0;
 
@@ -32,14 +39,15 @@ function playRound(playerSelection, computerSelection) {
     if ((playerSelection === "paper" && computerSelection === "rock") ||
         (playerSelection === "rock" && computerSelection === "scissor") ||
         (playerSelection === "scissor" && computerSelection === "paper")) {
-        playerScore++;
+        playerScore += .5;
         return "Congrats, you won this round!";
     } else if (playerSelection === computerSelection) {
         return "Let's try that again."
     } else {
-        computerScore++;
+        computerScore += .5;
         return "I got you this round!";
     }
 }    
 
 game();
+endGame();
