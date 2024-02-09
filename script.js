b6fa58668dabafb5e099881cf05bad3491fc7b59
KingFryRice <graysonbeatonrice@gmail.com>
2024-02-09 10:56:09 -0800

    Fix function call to play round after player selection

--- script.js
+++ script.js
@@ -5,10 +5,12 @@ const playerScoreKeeper = document.querySelector(".player-score");
 const computerScoreKeeper = document.querySelector(".computer-score");
 const endGameResult = document.querySelector(".endgame");
 
-let playerSelection
+let playerSelection;
 let computerSelection;
 let playerScore = 0;
 let computerScore = 0;
+
+playGame();
 
 const computerChoices = ["rock", "paper", "scissor"];
 function getComputerSelection() {
@@ -16,10 +18,18 @@ function getComputerSelection() {
     return computerSelection;
 }
 
-function getPlayerSelection() {
+function playGame() {
     buttons.forEach(button => {
     button.addEventListener("click", () =>  {
-        playerSelection = button.className;
+        if (playerScore === 3) {
+            endGameResult.textContent = "Great job you've bested me! Refresh to play again, I won't lose this time!.";
+        } else if (computerScore === 3) {
+            endGameResult.textContent =  "Better luck next time, refresh if you want to try again!";
+        } else {
+            playerSelection = button.className;
+            playRound(playerSelection);
+            choice.textContent = "Computer chose: " + computerSelection;
+        };
         });
     });
 };
@@ -29,32 +39,14 @@ function playRound(playerSelection) {
     if ((playerSelection === "paper" && computerSelection === "rock") ||
         (playerSelection === "rock" && computerSelection === "scissor") ||
         (playerSelection === "scissor" && computerSelection === "paper")) {
-        playerScore ++;
+        ++playerScore;
         result.textContent =  "Congrats, you won this round!";
     } else if (playerSelection === computerSelection) {
         result.textContent =  "Let's try that again.";
     } else {
-        computerScore ++;
+        ++computerScore;
         result.textContent = "I got you this round!";
     }
     playerScoreKeeper.textContent = playerScore;
     computerScoreKeeper.textContent = computerScore;
 }    
-
-function game() {
-    do {
-        playRound();
-        choice.textContent = "Computer chose: " + computerSelection;
-    } while (playerScore < 3 && computerScore < 3);
-}
-    
-function endGame() {
-    if (playerScore === 3) {
-        endGameResult.textContent = "Great job you've bested me! Refresh to play, I won't lose again!.";
-    } if (computerScore === 3) 
-        endGameResult.textContent =  "Better luck next time, refresh if you want to try again!";
-}
-
-game();
-endGame();
-
