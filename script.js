376f2dab2d9b9832a4fc4e72df87f683f9fd61c9
KingFryRice <graysonbeatonrice@gmail.com>
2024-02-08 17:32:52 -0800

    Update selection functions and restructure function order

--- script.js
+++ script.js
@@ -1,53 +1,67 @@
-const computerChoices = ["rock", "paper", "scissor"];
-
-function getComputerChoice() {
-    return computerChoices [Math.floor(Math.random() * computerChoices.length)];
-}
-
 const buttons = document.querySelectorAll("button");
+const choice = document.querySelector(".choice");
+const result = document.querySelector(".round-result");
+const playerScoreKeeper = document.querySelector(".player-score");
+const computerScoreKeeper = document.querySelector(".computer-score");
+const endGameResult = document.querySelector(".endgame");
 
-function getPlayerChoice() {
-    buttons.forEach(button => {
-    button.addEventListener("click", () =>  {
-        return button.className;
-        });
-    });
-}
-
-let playerSelection = getPlayerChoice();
-let computerSelection = getComputerChoice();
+let playerSelection
+let computerSelection;
 let playerScore = 0;
 let computerScore = 0;
+
+const computerChoices = ["rock", "paper", "scissor"];
+function getComputerSelection() {
+    computerSelection = computerChoices [Math.floor(Math.random() * computerChoices.length)];
+    return computerSelection;
+}
+
+function getPlayerSelection() {
+    buttons.forEach(button => {
+    button.addEventListener("click", () =>  {
+        playerSelection = button.className;
+        return playerSelection;
+        });
+    });
+};
+
+function getSelections() {
+    getPlayerSelection();
+    getComputerSelection();
+    console.log(playerSelection);
+    console.log(computerSelection);
+}
 
 function playRound(playerSelection, computerSelection) {
     if ((playerSelection === "paper" && computerSelection === "rock") ||
         (playerSelection === "rock" && computerSelection === "scissor") ||
         (playerSelection === "scissor" && computerSelection === "paper")) {
         playerScore ++;
-        return "Congrats, you won this round!";
+        result.textContent =  "Congrats, you won this round!";
     } else if (playerSelection === computerSelection) {
-        return "Let's try that again."
+        result.textContent =  "Let's try that again.";
     } else {
         computerScore ++;
-        return "I got you this round!";
+        result.textContent = "I got you this round!";
     }
 }    
 
 function game() {
     do {
         playRound();
-        console.log("Computer chose: " + computerSelection);
-        console.log(playRound(playerSelection, computerSelection));
-        console.log("Player Score: " + playerScore + " | Computer Score " + computerScore);
+        choice.textContent = "Computer chose: " + computerSelection;
     } while (playerScore < 3 && computerScore < 3);
 }
     
 function endGame() {
     if (playerScore === 3) {
-        console.log("Great job you've bested me! Refresh to play, I won't lose again!.");
+        endGameResult.textContent = "Great job you've bested me! Refresh to play, I won't lose again!.";
     } if (computerScore === 3) 
-        console.log("Better luck next time, refresh if you want to try again!");
+        endGameResult.textContent =  "Better luck next time, refresh if you want to try again!";
 }
 
-game();
+getSelections();
+//playRound();
+//game();
 //endGame();
+
